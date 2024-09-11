5d88e9390c27e79c74e7102f4941a027885b51aa
Siddharth Narayanan <sidnarayanan@users.noreply.github.com>
2024-09-10 17:54:10 -0700

    Merging identical nodes (#22)

--- tests/test_rollouts.py
+++ tests/test_rollouts.py
@@ -15,7 +15,7 @@ from ldp.alg.beam_search import BeamSearchRollout
 from ldp.alg.callbacks import Callback
 from ldp.alg.rollout import RolloutManager
 from ldp.alg.tree_search import TreeSearchRollout
-from ldp.data_structures import Trajectory, Transition, TransitionTree
+from ldp.data_structures import Trajectory, Transition
 from ldp.graph.common_ops import FxnOp
 from ldp.graph.op_utils import compute_graph, set_training_mode
 from ldp.graph.ops import OpResult
@@ -299,46 +299,3 @@ class TestTreeSearch:
         for traj in trajs:
             # should have hit target reward immediately
             assert len(traj.steps) == 1
-
-
-def test_tree_mc_value():
-    root_id = "dummy"
-    tree = TransitionTree(root_id=root_id)
-
-    kw = {
-        "agent_state": None,
-        "next_agent_state": None,
-        "observation": Transition.NO_OBSERVATION,
-        "next_observation": Transition.NO_OBSERVATION,
-        "action": None,
-    }
-
-    # Construct a tree with some rewards scattered about
-    tree.add_transition(f"{root_id}:0", Transition(timestep=0, reward=0.0, **kw))
-
-    tree.add_transition(f"{root_id}:0:0", Transition(timestep=1, reward=1.0, **kw))
-    for i in range(3):
-        tree.add_transition(
-            f"{root_id}:0:0:{i}",
-            Transition(timestep=2, reward=float(i), done=True, **kw),
-        )
-
-    tree.add_transition(
-        f"{root_id}:0:1", Transition(timestep=1, reward=-1.0, done=True, **kw)
-    )
-
-    tree.assign_mc_value_estimates(discount_factor=0.9)
-
-    # Now make sure the value estimates are as expected
-    # First, check the terminal nodes: Q==reward
-    for i in range(3):
-        assert tree.get_transition(f"{root_id}:0:0:{i}").value == float(i)
-    assert tree.get_transition(f"{root_id}:0:1").value == -1.0
-
-    # Then go up the tree
-    assert tree.get_transition(f"{root_id}:0:0").value == pytest.approx(
-        1 + 0.9 * ((0 + 1 + 2) / 3), rel=0.001
-    )
-    assert tree.get_transition(f"{root_id}:0").value == pytest.approx(
-        0.0 + 0.9 * ((1.9 - 1) / 2), rel=0.001
-    )
