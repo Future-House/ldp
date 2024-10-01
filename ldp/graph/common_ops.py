9dde1926dc738a2b37261f3af090b6ea9dc740b2
Albert Bou <albertbou92@users.noreply.github.com>
2024-10-01 09:39:29 -0700

    Update ldp/graph/common_ops.py
    Co-authored-by: Siddharth Narayanan <sidnarayanan@users.noreply.github.com>

--- ldp/graph/common_ops.py
+++ ldp/graph/common_ops.py
@@ -388,8 +388,7 @@ class EmbeddingOp(Op):
         sparse_embedding_dim: int = 0,
         **embedding_model_kwargs,
     ):
-        if "timeout" not in embedding_model_kwargs:
-            embedding_model_kwargs["timeout"] = 60.0
+        embedding_model_kwargs.setdefault("timeout", 60.0)
         emb_models: list[EmbeddingModel] = []
         if dense_embedding_dim > 0:
             emb_models.append(
