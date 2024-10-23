571ac6ec5f9be839b28a586ce5e44365cc16c2aa
Siddharth Narayanan <sidnarayanan@users.noreply.github.com>
2024-10-23 14:25:44 -0700

    Mark truncated earlier (#118)

--- ldp/alg/rollout.py
+++ ldp/alg/rollout.py
@@ -237,6 +237,11 @@ class RolloutManager:
             for timestep in itertools.count():
                 step = await self._take_step(timestep, traj_id, env, agent_state, obs)
 
+                if timestep + 1 == max_steps and not step.done:
+                    # Mark as truncated if we hit max_steps and the state is not terminal.
+                    # Do it before store_step(), so that callbacks can access this info
+                    step.truncated = True
+
                 # We assume the below won't throw a CaughtError
                 await store_step(step)
 
@@ -244,11 +249,7 @@ class RolloutManager:
                 agent_state = step.next_agent_state
                 obs = step.next_observation
 
-                if step.done:
-                    break
-
-                if timestep + 1 == max_steps:
-                    trajectory.steps[-1].truncated = True
+                if step.done or step.truncated:
                     break
 
         except CaughtError as e:
