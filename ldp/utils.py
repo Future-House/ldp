f249dfb67f18d6d76c4da1417f4be7347b441f02
Mayk Caldas <mayk@futurehouse.org>
2024-12-11 13:53:03 -0800

    Moved configure_default_log to llmclient and bumped its version

--- ldp/utils.py
+++ ldp/utils.py
@@ -1,37 +1,6 @@
 import logging
 import logging.config
-import os
 from typing import Any
-
-import litellm
-
-
-def configure_log_levels() -> None:
-    """Configure log levels."""
-    # Set sane default LiteLLM logging configuration
-    # SEE: https://docs.litellm.ai/docs/observability/telemetry
-    litellm.telemetry = False
-    if (
-        logging.getLevelNamesMapping().get(
-            os.environ.get("LITELLM_LOG", ""), logging.WARNING
-        )
-        < logging.WARNING
-    ):
-        # If LITELLM_LOG is DEBUG or INFO, don't change the LiteLLM log levels
-        litellm_loggers_config: dict[str, Any] = {}
-    else:
-        litellm_loggers_config = {
-            "LiteLLM": {"level": "WARNING"},
-            "LiteLLM Proxy": {"level": "WARNING"},
-            "LiteLLM Router": {"level": "WARNING"},
-        }
-
-    logging.config.dictConfig({
-        "version": 1,
-        "disable_existing_loggers": False,
-        # Lower level for httpx and LiteLLM
-        "loggers": {"httpx": {"level": "WARNING"}} | litellm_loggers_config,
-    })
 
 
 def configure_stdout_logs(
