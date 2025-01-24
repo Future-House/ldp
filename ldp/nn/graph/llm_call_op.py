33f898fd5365ba5bc898a74ab484d0ddc94731c2
Siddharth Narayanan <sid@futurehouse.org>
2025-01-24 14:55:24 -0800

    make message input/output configurabe

--- ldp/nn/graph/llm_call_op.py
+++ ldp/nn/graph/llm_call_op.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
 import json
+from collections.abc import Callable
 from functools import partial
 from typing import Any, ClassVar
 
 import tree
-from aviary.core import MalformedMessageError, Message, Messages
-from aviary.tools import Tool, ToolCall, ToolRequestMessage
+from aviary.core import (
+    MalformedMessageError,
+    Message,
+    Messages,
+    ToolCall,
+    ToolRequestMessage,
+    Tools,
+)
 from transformers import LogitsProcessorList
 
 from ldp.graph.gradient_estimators import assign_constant_grads
@@ -24,6 +31,76 @@ from ..handlers.transformer_handler import (  # noqa: TID252
 from ..lm_config import LMConfig  # noqa: TID252
 
 
+def default_prep_messages_for_tokenizer(msgs: Messages) -> list[dict]:
+    """A default messages prep function following the Llama 3.1 syntax."""
+    result: list[dict] = []
+    for msg in msgs:
+        content = msg.content
+        if isinstance(msg, ToolRequestMessage):
+            assert len(msg.tool_calls) == 1, (
+                "Support parsing only single tool call for now"
+            )
+            tool_call = msg.tool_calls[0]
+            # TODO: document where this format is coming from. Is this a Huggingface chat template syntax?
+            content_dict = {
+                "name": tool_call.function.name,
+                "parameters": tool_call.function.arguments,
+                "thought": msg.content,
+            }
+            content = json.dumps(content_dict)
+        assert content is not None, "content is None, doesn't make sense"
+
+        result.append({"role": msg.role, "content": content})
+    return result
+
+
+def default_prep_tools_for_tokenizer(tools: Tools | None) -> list[dict] | None:
+    """A default tools prep function following the Llama 3.1 syntax."""
+    if not tools:
+        return None
+
+    # TODO: should be able to switch to tool.info.model_dump() here
+    return [
+        {
+            "name": tool.info.name,
+            "description": tool.info.description,
+            "parameters": {
+                "type": tool.info.parameters.type,
+                "properties": {
+                    prop_name: {
+                        "type": prop_details.get("type"),
+                        "description": prop_details.get("description"),
+                        "title": prop_details.get("title"),
+                    }
+                    for prop_name, prop_details in tool.info.parameters.properties.items()
+                },
+                "required": tool.info.parameters.required,
+            },
+        }
+        for tool in tools
+    ]
+
+
+def default_parse_tool_request_message(
+    out_text: str, tools: Tools
+) -> ToolRequestMessage:
+    """A default tool request message parsing following the Llama 3.1 syntax."""
+    try:
+        tool_request = json.loads(out_text)
+        tool_name = tool_request["name"]
+        tool = next(t for t in tools if t.info.name == tool_name)
+        tool_thought = tool_request.get("thought", "")
+        tool_parameters = tool_request.get("parameters", {})
+        return ToolRequestMessage(
+            tool_calls=[ToolCall.from_tool(tool, **tool_parameters)],
+            content=tool_thought,
+        )
+    except StopIteration as exc:
+        raise MalformedMessageError(f"Tool {tool_name} not found in tools.") from exc
+    except json.JSONDecodeError as err:
+        raise ValueError(f"Failed to parse tools call message: {out_text}") from err
+
+
 class LocalLLMCallOp(Op[Message]):
     """An Op that samples a token sequence from a local language model."""
 
@@ -39,6 +116,15 @@ class LocalLLMCallOp(Op[Message]):
         batch_size: int = 1,
         max_wait_interval: float = 0.1,
         parallel_mode_config: ParallelModeConfig | None = None,
+        prep_messages_for_tokenizer: Callable[
+            [Messages], list[dict]
+        ] = default_prep_messages_for_tokenizer,
+        prep_tools_for_tokenizer: Callable[
+            [Tools | None], list[dict] | None
+        ] = default_prep_tools_for_tokenizer,
+        parse_tool_request_message: Callable[
+            [str, Tools], ToolRequestMessage
+        ] = default_parse_tool_request_message,
     ) -> None:
         super().__init__()
 
@@ -61,91 +147,22 @@ class LocalLLMCallOp(Op[Message]):
         self.model_handler = handler_config.make_async_module()
         self.model_name = model_config.model
 
+        self.prep_messages_for_tokenizer = prep_messages_for_tokenizer
+        self.prep_tools_for_tokenizer = prep_tools_for_tokenizer
+        self.parse_tool_request_message = parse_tool_request_message
+
         self.llm_call_kwargs = {"logits_processor": LogitsProcessorList()}
-
-    @staticmethod
-    def prep_messages_for_tokenizer(xi: Messages) -> list[dict]:
-        result: list[dict] = []
-        for msg in xi:
-            content = msg.content
-            if isinstance(msg, ToolRequestMessage):
-                assert len(msg.tool_calls) == 1, (
-                    "Support parsing only single tool call for now"
-                )
-                tool_call = msg.tool_calls[0]
-                # TODO: document where this format is coming from. Is this a Huggingface chat template syntax?
-                content_dict = {
-                    "name": tool_call.function.name,
-                    "parameters": tool_call.function.arguments,
-                    "thought": msg.content,
-                }
-                content = json.dumps(content_dict)
-            assert content is not None, "content is None, doesn't make sense"
-
-            result.append({"role": msg.role, "content": content})
-        return result
-
-    @staticmethod
-    def prep_tools_for_tokenizer(tools: list[Tool] | None) -> list[dict] | None:
-        """Prepare tools for the tokenizer by transforming them into a JSON schema."""
-        if not tools:
-            return None
-
-        # TODO: should be able to switch to tool.info.model_dump() here
-        return [
-            {
-                "name": tool.info.name,
-                "description": tool.info.description,
-                "parameters": {
-                    "type": tool.info.parameters.type,
-                    "properties": {
-                        prop_name: {
-                            "type": prop_details.get("type"),
-                            "description": prop_details.get("description"),
-                            "title": prop_details.get("title"),
-                        }
-                        for prop_name, prop_details in tool.info.parameters.properties.items()
-                    },
-                    "required": tool.info.parameters.required,
-                },
-            }
-            for tool in tools
-        ]
-
-    @staticmethod
-    def _parse_tool_request(out_text: str, tools: list[Tool]) -> ToolRequestMessage:
-        """Parse the output text to extract the tool request.
-
-        TODO: see if this needs to be configurable, e.g. for different model
-        output formats that we want to experiment with.
-        """
-        try:
-            tool_request = json.loads(out_text)
-            tool_name = tool_request["name"]
-            tool = next(t for t in tools if t.info.name == tool_name)
-            tool_thought = tool_request.get("thought", "")
-            tool_parameters = tool_request.get("parameters", {})
-            return ToolRequestMessage(
-                tool_calls=[ToolCall.from_tool(tool, **tool_parameters)],
-                content=tool_thought,
-            )
-        except StopIteration as exc:
-            raise MalformedMessageError(
-                f"Tool {tool_name} not found in tools."
-            ) from exc
-        except json.JSONDecodeError as err:
-            raise ValueError(f"Failed to parse tools call message: {out_text}") from err
 
     async def forward(
         self,
-        xi: list[Message],
+        msgs: list[Message],
         temperature: float = 1.0,
         max_new_tokens: int = 10,
-        tools: list[Tool] | None = None,
+        tools: Tools | None = None,
         **kwargs: dict[str, Any],
     ) -> Message:
         call_id = get_call_id()
-        inputs = self.prep_messages_for_tokenizer(xi)
+        inputs = self.prep_messages_for_tokenizer(msgs)
         tools_json = self.prep_tools_for_tokenizer(tools)
         if get_training_mode():
             self.ctx.update(call_id, LocalLLMCallOp.CTX_INPUTS_PREP_KEY, inputs)
@@ -166,7 +183,7 @@ class LocalLLMCallOp(Op[Message]):
 
         out_msg = Message(role="assistant", content=out_text)
         if tools and out_text.startswith("{"):
-            out_msg = self._parse_tool_request(out_text, tools)
+            out_msg = self.parse_tool_request_message(out_text, tools)
 
         if get_training_mode():
             self.ctx.update(
