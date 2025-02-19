38d787149d520a69d5992538b298fc9277bede1e
maykcaldas <mayk@futurehouse.org>
2025-02-19 12:28:16 -0800

    formatted cassets

--- ldp/graph/ops.py
+++ ldp/graph/ops.py
@@ -69,7 +69,6 @@ class OpResult(Generic[TOutput_co]):
         cls, t_output: type[TOutput_co], dump: dict[str, Any]
     ) -> OpResult[TOutput_co]:
         value = dump.pop("value")
-        logprob = dump.pop("logprob", None)
         if issubclass(t_output, BaseModel):
             value = t_output.model_validate(value)
 
