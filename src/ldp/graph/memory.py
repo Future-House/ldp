c27fed800883bc471f8bd141c1f10405b2966277
James Braza <jamesbraza@gmail.com>
2025-06-13 11:06:36 -0700

    Removed now unnecessary typing cast/mypy disables

--- src/ldp/graph/memory.py
+++ src/ldp/graph/memory.py
@@ -51,7 +51,7 @@ class Memory(BaseModel):
         description="Measure of the output's quality (e.g. loss)."
     )
     metadata: JsonValue = Field(
-        default_factory=dict,  # type: ignore[arg-type] # SEE: https://github.com/pydantic/pydantic/issues/10950
+        default_factory=dict,
         description=(
             "Optional JSON metadata to store with the memory. An example is storing"
             " information an optimizer can use at training time."
