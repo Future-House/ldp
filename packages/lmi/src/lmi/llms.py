e881d747911db1669480e720e5dce37e8a1e51af
Andrew White <white.d.andrew@gmail.com>
2025-07-09 11:37:20 -0700

    Add convenience for call single

--- packages/lmi/src/lmi/llms.py
+++ packages/lmi/src/lmi/llms.py
@@ -365,7 +365,7 @@ class LLMModel(ABC, BaseModel):
 
     async def call_single(
         self,
-        messages: list[Message],
+        messages: list[Message] | str,
         callbacks: Iterable[Callable] | None = None,
         name: str | None = None,
         output_type: type[BaseModel] | TypeAdapter | JSONSchema | None = None,
@@ -373,8 +373,18 @@ class LLMModel(ABC, BaseModel):
         tool_choice: Tool | str | None = TOOL_CHOICE_REQUIRED,
         **kwargs,
     ) -> LLMResult:
+        if type(messages) is str:
+            # convenience for single message
+            messages = [Message(role="user", content=messages)]
         results = await self.call(
-            messages, callbacks, name, output_type, tools, tool_choice, n=1, **kwargs
+            cast(list[Message], messages),
+            callbacks,
+            name,
+            output_type,
+            tools,
+            tool_choice,
+            n=1,
+            **kwargs,
         )
         if len(results) != 1:
             # Can be caused by issues like https://github.com/BerriAI/litellm/issues/12298
