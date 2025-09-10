b7437989953e9d5e749516f35fa3fff2c8cbc289
albertbou <albertbou92@gmail.com>
2025-09-10 15:40:03 -0700

    fix mypy

--- src/ldp/nn/handlers/transformer_handler.py
+++ src/ldp/nn/handlers/transformer_handler.py
@@ -690,7 +690,7 @@ class ParallelAsyncTransformer(AsyncTransformerInterface):
         results = self._client_gather(futures)
 
         if split_data:
-            return chunker.dechunkify(cast(list[TOutputType], results), dummy_flags)
+            return chunker.dechunkify(results, dummy_flags)
         return results
 
     def wrap_afunc(
